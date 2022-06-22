5d5d6ce7dd39275370b743b780409c6b537c5739
ArtemKozak <a.s.kozak@ukr.net>
2022-06-22 10:27:39 +0300

    rick/morty

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div>
 
+      {/*Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі*/}
+      {/*https://rickandmortyapi.com/*/}
+      {/*https://rickandmortyapi.com/api/character*/}
+      {/*Створити 6 персонажів*/}
+
+
     </div>
   );
 }
