adf68dd25e5e937d3649f82c774d259efc04dcf9
ArtemKozak <a.s.kozak@ukr.net>
2022-06-23 11:57:46 +0300

    rick/morty

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import Npc from "./components/Npc";
 
 function App() {
 
-    let  [npc, setNpc] = useState([]);
+    let [npc, setNpc] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
             .then(value => value.json())
             .then(value => {
                 setNpc(value.results);
                 console.log(value)
             })
-    },[])
+    }, [])
 
 
     return (
         <div>
             {
-                npc.map(value => <Npc name={value.name}/>
+                npc.map(value => <Npc id={value.id}
+                                      name={value.name}
+                                      status={value.status}
+                                      species={value.species}
+                                      gender={value.gender}
+                                      image={value.image}
+                    />
                 )
             }
 
