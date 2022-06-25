07de509e6874ff326dc2bcabca7bbe4df83e41e8
ArtemKozak <a.s.kozak@ukr.net>
2022-06-25 19:56:57 +0300

    hw2_spaceX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div>
 
-    </div>
+  let [flights, setFlights] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/launches/')
+        .then(value => value.json())
+        .then(value => {
+            setFlights(value.filter(flight => flight.launch_year !== '2020'));
+        })
+  }, [])
+
+
+
+  return (
+
   );
 }
 
