4ee34b3b1bc9fdf6e12d89df7c4461cbf31f7b82
ArtemKozak <a.s.kozak@ukr.net>
2022-06-27 15:31:10 +0300

    hw2_spaceX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import './App.css';
 import {useEffect, useState} from "react";
+import Flight from "./Components/Flight";
 
 function App() {
 
-  let [flights, setFlights] = useState([])
+    let [flights, setFlights] = useState([])
 
-  useEffect(() => {
-    fetch('https://api.spacexdata.com/v3/launches/')
-        .then(value => value.json())
-        .then(value => {
-            setFlights(value.filter(flight => flight.launch_year !== '2020'));
-        })
-  }, [])
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(value => value.json())
+            .then(value => {
+                setFlights(value.filter(flight => flight.launch_year !== '2020'));
+            })
+    }, [])
 
 
+    return (
+        <div>
+            <h2>SpaseX</h2>
+            {flights.map((flight, index) => <Flight
+                key={index}
+                item={flight}
+            />)}
 
-  return (
-
-  );
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Flight.js
@@ -0,0 +1,11 @@
+import './Flight.css'
+export default function Flight({item}) {
+
+
+
+    return (<div className={'spaceX'}>
+        {item.flight_number}. {item.mission_name}  year: {item.launch_year}
+        <img src={item.links.mission_patch_small} alt="link"/>
+
+    </div>)
+}
