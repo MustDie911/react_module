03bbbdc8975855c523ae83b998f3647223994089
ArtemKozak <a.s.kozak@ukr.net>
2022-07-15 11:02:40 +0300

    react-hook-form + axios

--- src/services/comments.form.service.js
+++ src/services/comments.form.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let axiosInstance = axios.create({
-    baseURL: "http://jsonplaceholder.typicode.com/comments"
+    baseURL: "https://jsonplaceholder.typicode.com/comments"
 });
 
 const getComments = (post) => axiosInstance.post('', post);
